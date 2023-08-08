e0f089d0d1216ab27d13cf7e8c64d05baf2cddd4
Berke <93223660+quatronostro@users.noreply.github.com>
2023-08-09 01:43:20 +0300

    day02/02

--- cypress/e2e/0-MyCypress/day02_locators.cy.js
+++ cypress/e2e/0-MyCypress/day02_locators.cy.js
@@ -2,6 +2,18 @@ describe('taking locators', () => {
     it('cy.get command', () => {
         cy.visit('/')
         cy.get('#APjFqb').type('cypress{enter}') // sendkeys
-        cy.get('.MUFPAc > :nth-child(2) > a').click
+        //cy.get('[style="position:absolute;right:11px;top:13px;color:inherit"]').click() // not working
+        //cy.get('.MUFPAc > :nth-child(2) > a') // its not working!!!
+
+        cy.get('.MUFPAc > :nth-child(2) >>').click()
+        
+        cy.get('.F1hUFe').click()
+        cy.get('#APjFqb').type('nutella{enter}') // sendkeys
+        cy.get('.MUFPAc > :nth-child(2) >>').click()
+        cy.get('.F1hUFe').click()
+
+        cy.get('#APjFqb').type('samsung{enter}') // sendkeys
+        cy.get('.CCgQ5 > span')
+        cy.go('back')
     });
 });
